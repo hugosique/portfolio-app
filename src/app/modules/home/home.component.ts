72ab07b1b50eee124b8ae8184cbfe28de552d265
hugosique <otahugo02@gmail.com>
2024-02-05 14:55:37 -0300

    social media buttons addition

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -6,21 +6,5 @@ import { ScrollService } from 'src/app/shared/services/scroll.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements AfterViewInit {
-
-  constructor(
-    private scrollService: ScrollService,
-    private el: ElementRef,
-    private renderer: Renderer2
-  ) {}
-
-  ngAfterViewInit() {
-    this.scrollService.scroll$.subscribe(sectionId => {
-      const element = this.el.nativeElement.querySelector(`#${sectionId}`);
-      if (element) {
-        this.renderer.setProperty(document.documentElement, 'scrollTop', element.offsetTop);
-      }
-    });
-  }
-
+export class HomeComponent {
 }
